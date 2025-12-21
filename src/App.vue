05f2aaf3faa14c792480607229b275318994cefa
ka-dim <kart.09-ducky@icloud.com>
2025-12-21 23:21:44 +0000

    Listing Jobs completed

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <script setup>
+  import { ref } from 'vue';
   import Hero from './components/Hero.vue';
-import HomeCards from './components/HomeCards.vue';
+  import HomeCards from './components/HomeCards.vue';
+  import JobListings from './components/JobListings.vue';
   import NavBar from './components/NavBar.vue';
-
-
 
 </script>
 
 <template>
   <NavBar />
   <Hero />
-  <HomeCards/>
+  <HomeCards />
+  <JobListings :limit="3" :showButton='true' />
 </template>
 
 <style scoped>

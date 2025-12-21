b1aad0f126ec750d1396b80c0439e8ccc1432685
ka-dim <kart.09-ducky@icloud.com>
2025-12-21 18:33:41 +0000

    tailwindcss setup like on the tuto

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+      gridTemplateColumns: {
+        "70/30": "70% 28%",
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],

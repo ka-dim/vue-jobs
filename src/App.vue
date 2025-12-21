7d87687f1020a8b7edcb7f0ee18890bf670c3c07
ka-dim <kart.09-ducky@icloud.com>
2025-12-21 19:08:20 +0000

    Navbar and Hero done

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
+  import Hero from './components/Hero.vue';
+  import NavBar from './components/NavBar.vue';
+
+
 
 </script>
 
 <template>
-  <h1>Vue Jobs</h1>
+  <NavBar />
+  <Hero />
 </template>
 
 <style scoped>

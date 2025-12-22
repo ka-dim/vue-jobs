8103d7fc4de609cabaa45eb464117ad6504c8e39
ka-dim <kart.09-ducky@icloud.com>
2025-12-22 10:47:30 +0000

    Not Found Page done

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import HomePage from "@/views/HomePage.vue";
 import JobsView from "@/views/JobsView.vue";
+import NotFoundPage from "@/views/NotFoundPage.vue";
 
 const routes = [
   {
@@ -13,6 +14,11 @@ const routes = [
     path: "/jobs",
     name: "jobsView",
     component: JobsView,
+  },
+  {
+    path: "/:pathMatch(.*)",
+    name: "pageNotFound",
+    component: NotFoundPage,
   },
 ];
 
